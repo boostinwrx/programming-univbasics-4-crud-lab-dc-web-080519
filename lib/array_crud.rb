15191e5eff3082f26a45a68ec8f381706aae748d
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-07-22 06:15:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,23 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  p array = ["1","2","3","4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Jim","Tom"]
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [1,2,3]
+  array.unshift(${element})
 end
 
 def remove_element_from_end_of_array(array)
-  
+ 
 end
 
 def remove_element_from_start_of_array(array)
